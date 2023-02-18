1c1a10c1fd0eb4fd04d4f90a1940c285d99b90c1
prince475 <danielmwai475@gmail.com>
2023-02-18 17:45:57 +0300

    Completed Monkey Patching #counting sentences lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,33 @@
 require 'pry'
 
+require 'pry'
+
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = self.split(/[!.?]/)
+    count.delete_if{|str| str == ""}
+    count.size
   end
 end
+
+# additional practice
+
+def contains_vowel(str)
+  str =~ /[aeiou]/
+end
+
+contains_vowel("testament") # returns 1
+contains_vowel("sky")
